d550cda6c546c5f52f8255b33611746a536d186c
DLaaren <dlaaren@yandex.ru>
2023-03-21 14:03:18 +0700

    getting ready to create factory for commands

--- calculator/src/main/java/ru/nsu/fit/vinter/calculator/core/Calculator.java
+++ calculator/src/main/java/ru/nsu/fit/vinter/calculator/core/Calculator.java
@@ -1,6 +1,7 @@
 package ru.nsu.fit.vinter.calculator.core;
 
 public class Calculator {
+    private CommandFactory factory = new CommandFactory();
     private ExecutionContext calcContext = new ExecutionContext();
     //Scanner scanner; //встроенный
 }
--- calculator/src/main/java/ru/nsu/fit/vinter/calculator/core/CommandFactory.java
+++ calculator/src/main/java/ru/nsu/fit/vinter/calculator/core/CommandFactory.java
@@ -1,5 +1,25 @@
 package ru.nsu.fit.vinter.calculator.core;
 
+import ru.nsu.fit.vinter.calculator.executable.Main;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 public class CommandFactory {
+    private Map<String, Command> commands = new HashMap<>();
+
+    public Command createCommand(ExecutionContext context, String[] args) {
+        InputStream in = Main.class.getResourceAsStream("command.properties");
+        Properties properties = new Properties();
+        properties.load(in);
+
+        String className = ;
+        Class<?> commandClass = Class.forName(className);
+        Command command = commandClass.forName(className).newInstance();
+
+        return command;
+    }
 
 }
