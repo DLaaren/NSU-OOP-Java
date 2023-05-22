5cef692897b207b1ade68c676e242565b76bdb61
DLaaren <dlaaren@yandex.ru>
2023-05-22 15:59:47 +0700

    That is not out lab. It is just tries to understand the netty. Писала-писала, а теперь попёрла на сдачу последней лабы по осям

---
+++ chat/src/main/java/ru/nsu/fit/vinter/chat/core/MainHandler.java
@@ -0,0 +1,8 @@
+package ru.nsu.fit.vinter.chat.core;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+//обработчик входящих данных
+public class MainHandler extends ChannelInboundHandlerAdapter {
+
+}
--- chat/src/main/java/ru/nsu/fit/vinter/chat/executable/Main.java
+++ chat/src/main/java/ru/nsu/fit/vinter/chat/executable/Main.java
@@ -1,7 +1,45 @@
 package ru.nsu.fit.vinter.chat.executable;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Обработка в параллельных потоках -- менеджеры потоков // пуллы потоков
+
+        //подключение/инициализация клиентов клиентов
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+
+        //обработка данных / сетевое взаимодействие
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap chatServer = new ServerBootstrap();
+            chatServer.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        //socket channel contains all data about connection
+                        //we have initialized channel for connection
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+                        }
+                    });
+            //привязываемся к порту и запускаем сервер
+            ChannelFuture future = chatServer.bind(8189).sync();
+            //blocking operation // we are waiting for server to be closed;
+            future.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //closing threadpools
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 }
